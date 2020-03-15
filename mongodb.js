92f3cf92b998921fe52fad9ddd9db8cecde17b47
Fabiano Netto <fabianonetto@gmail.com>
2020-03-15 18:04:27 -0400

    End of Lesson 76

--- mongodb.js
+++ mongodb.js
@@ -11,8 +11,53 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error,client) => {
     
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Fabiano',
-        age: 36
+    // db.collection('users').insertOne({
+    //     name: 'Fabiano',
+    //     age: 36
+    // }, (error,result) => {
+    //     if(error){
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     },
+    //     {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ],(error,result) => {
+    //     if(error){
+    //         return console.log('Unable to insert users')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Do Groceries',
+            completed: false
+        },
+        {
+            description: 'Do Laundry',
+            completed: true
+        },
+        {
+            description: 'Do Dishes',
+            completed: false
+        }
+    ], (error,result) => {
+        if (error){
+            return console.log('Unable to insert tasks')
+        }
+
+        console.log(result.ops)
     })
+
 })
