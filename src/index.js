3e3d87233f4f42c86a4854d5e68bf504dd60f65e
Fabiano Netto <fabianonetto@gmail.com>
2020-03-19 17:37:29 -0400

    End of Lesson 98

--- src/index.js
+++ src/index.js
@@ -19,6 +19,27 @@ app.post('/users',async (req,res) => {
     }
 })
 
+app.patch('/users/:id',async (req,res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name','email','password','age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if(!isValidOperation){
+        return res.status(400).send('error: Invalid Updates')
+    }
+
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id,req.body,{new: true, runValidators: true})
+
+        if(!user) {
+            res.status(404).send()
+        }
+        res.send(user)
+    }catch(e) {
+        res.status(400).send(e)
+    }    
+})
+
 app.get('/users',async (req,res) => {
     try{
         res.send(await User.find({}))
