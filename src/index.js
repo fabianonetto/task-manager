ceaed18439a8a417af6d7e1a7bd1f2bef9a5684a
Fabiano Netto <fabianonetto@gmail.com>
2020-03-19 20:08:16 -0400

    End of Lesson 100
    Implemented Delete methods

--- src/index.js
+++ src/index.js
@@ -40,6 +40,20 @@ app.patch('/users/:id',async (req,res) => {
     }    
 })
 
+app.delete('/users/:id', async (req,res) => {
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if(!user){
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
+
 app.get('/users',async (req,res) => {
     try{
         res.send(await User.find({}))
@@ -87,6 +101,20 @@ app.patch('/tasks/:id',async (req,res) => {
         }
         res.send(task)
     }catch(e){
+        res.status(500).send(e)
+    }
+})
+
+app.delete('/tasks/:id',async (req,res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if(!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    }catch(e) {
         res.status(500).send(e)
     }
 })
