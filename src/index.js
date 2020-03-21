16d96185e7db22957970c26bc3dcf0d42eebd369
Fabiano Netto <fabianonetto@gmail.com>
2020-03-20 20:11:27 -0400

    End of lesson 107

--- src/index.js
+++ src/index.js
@@ -15,15 +15,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-
-const jwt = require('jsonwebtoken')
-
-const myFunction = async() => {
-    const token = jwt.sign({_id: 'abc123'},'thisismynewcourse',{expiresIn: '7 days'})
-    console.log(token)
-
-    console.log(jwt.verify(token,'thisismynewcourse'))
-}
-
-myFunction()
