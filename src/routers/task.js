b914ade2f5c041e0e2405995acb6d1c103d0a8ec
Fabiano Netto <fabianonetto@gmail.com>
2020-03-24 13:24:27 -0400

    End of Lesson 121

--- src/routers/task.js
+++ src/routers/task.js
@@ -64,11 +64,18 @@ router.delete('/tasks/:id', auth, async (req, res) => {
 
 //Get /tasks?completed=true
 //Get /tasks?limit=1&skip=20
+//Get  /tasks?sortBy=CreatedAt_asc
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
+    const sort = {}
 
     if(req.query.completed){
         match.completed = req.query.completed === 'true'
+    }
+
+    if(req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
 
     try {
@@ -77,7 +84,8 @@ router.get('/tasks', auth, async (req, res) => {
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate()
 
