ccdb5b47df141b08b04477117469aa7dc717a9cb
Fabiano Netto <fabianonetto@gmail.com>
2020-03-26 14:20:53 -0400

    Revert "End of Lesson 128"
    This reverts commit 3792a353406fb1afa2a434a25906e4033935d07c.

--- src/routers/user.js
+++ src/routers/user.js
@@ -55,21 +55,6 @@ router.delete('/users/me/avatar', auth, async (req,res) => {
     res.status(400).send({error: error.message})
 })
 
-router.get('/users/:id/avatar', async (req, res) => {
-    try{
-        const user = await User.findById(req.params.id)
-
-        if(!user || !user.avatar) {
-            throw new Error()
-        }
-
-        res.set('Content-Type','image/jpg')
-        res.send(user.avatar)
-    } catch(e) {
-        res.status(404).send()
-    }
-})
-
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
