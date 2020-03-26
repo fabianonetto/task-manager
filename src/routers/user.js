04f8ef27d24bb39e24f5d09895b2701c316d9c0c
Fabiano Netto <fabianonetto@gmail.com>
2020-03-26 17:29:26 -0400

    End of Lesson 129
    Auto Cropping and Image Formatting

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const multer = require('multer')
+const sharp = require('sharp')
 
 const upload = multer({
     limits: {
@@ -39,7 +40,9 @@ router.post('/users', async (req, res) => {
 })
 
 router.post('/users/me/avatar', auth , upload.single('avatar') ,async (req,res) => {
-    req.user.avatar = req.file.buffer
+    req.user.avatar = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer()
+    
+
     await req.user.save()
     res.send()
 },(error,req,res,next) => {
@@ -63,7 +66,7 @@ router.get('/users/:id/avatar', async (req, res) => {
             throw new Error()
         }
 
-        res.set('Content-Type','image/jpg')
+        res.set('Content-Type','image/png')
         res.send(user.avatar)
     } catch(e) {
         res.status(404).send()
