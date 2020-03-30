ff20f13f164e007705765e07790784a65a1ac3ee
Fabiano Netto <fabianonetto@gmail.com>
2020-03-30 00:59:20 -0400

    End of Lesson 139
    Writing Your Own Tests

--- src/math.js
+++ src/math.js
@@ -1,5 +1,16 @@
 const calculateTip = (total,tipPercemt = 0.2) => total + (total * tipPercemt)
 
+const fahrenheitToCelsius = (temp) => {
+    return (temp - 32) / 1.8
+}
+
+const celsiusToFahrenheit = (temp) => {
+    return (temp * 1.8) + 32
+}
+
+
 module.exports = {
-    calculateTip
+    calculateTip,
+    fahrenheitToCelsius,
+    celsiusToFahrenheit
 }
